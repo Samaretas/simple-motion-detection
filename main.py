0bdc72b448bd18f23ef259074011f7d0e68e59fa
Samaretas <samuele.conti@studenti.unitn.it>
2021-04-12 16:19:23 +0200

    One plus for each step

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from cv2 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 from motion_detection import MotionDetection
+
 
 def read_video_stream(name):
     frames = list()
     colored_frames = list()
-    cap= cv2.VideoCapture(name)
+    cap = cv2.VideoCapture(name)
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == False:
             break
         colored_frames.append(frame)
-        frames.append(np.array(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), dtype='uint8'))
+        frames.append(np.array(cv2.cvtColor(
+            frame, cv2.COLOR_RGB2GRAY), dtype='uint8'))
     cap.release()
     return frames, colored_frames
 
 
 frames, colored_frames = read_video_stream('rolling_resized.mp4')
+# frames, colored_frames = read_video_stream('Rolling-resized1over4.m4v')
 print(f'read {len(frames)} frames, of size {frames[0].shape}')
 
-detector = MotionDetection()
-detector.diamond_search_motion_estimation(frames[30], frames[31], colored_frames[30])
+fidx = 30
 
+detector = MotionDetection()
+motion_map = detector.diamond_search_motion_estimation(frames[fidx], frames[fidx+1])
+
+plt.subplot(2, 2, 1), plt.imshow(cv2.cvtColor(colored_frames[fidx], cv2.COLOR_BGR2RGB))
+plt.subplot(2, 2, 2), plt.imshow(cv2.cvtColor(colored_frames[fidx+1], cv2.COLOR_BGR2RGB))
+plt.title('Original')
+
+plt.subplot(2, 2, (3, 4)), plt.imshow(motion_map, 'gray')
+plt.title('Motion map')
+
+plt.show()
--- motion_detection.py
+++ motion_detection.py
@@ -1,6 +1,5 @@
 import numpy as np
 from cv2 import cv2
-import matplotlib.pyplot as plt
 import time
 from scipy.ndimage.filters import gaussian_filter
 
@@ -8,9 +7,10 @@ from scipy.ndimage.filters import gaussian_filter
 def timing(func):
     def wrap(*args, **kwargs):
         start = time.time()
-        func(*args, **kwargs)
+        ret = func(*args, **kwargs)
         end = time.time()
         print(f"Function executed in {int(end-start)}s")
+        return ret
     return wrap
 
 
@@ -43,27 +43,26 @@ class MotionDetection:
         return np.sum(np.abs(ba-bb))
 
     @timing
-    def diamond_search_motion_estimation(self, prev_frame, now_frame, original=None):
+    def diamond_search_motion_estimation(self, prev_frame, now_frame):
         """
         I assume here to get as input numpy arrays.
         """
         shape = prev_frame.shape
         motion_map = np.zeros_like(prev_frame, dtype='int16')
 
-        # notice here the approach at borders, it must be corrected
+        # notice here the approach at borders, at the moment we neglect right and bottom leftovers
         for row in range(shape[0]-self.block_size):
             for col in range(shape[1]-self.block_size):
                 # iterating over all image positions
-                motion_distance = -1
-
-                this_block = prev_frame[row:row +
-                                        self.block_size, col:col+self.block_size]
+                shift_distance = -1
+                
+                this_block = prev_frame[row:row + self.block_size, col:col+self.block_size]
                 match_position = (row, col)
 
                 # find the best large offset \w large diamond search
                 stopping_condition = False
                 while(not stopping_condition):
-                    motion_distance += 1
+                    shift_distance += 1
                     min_diff = float('inf')
                     best_pos = match_position
                     for offset in self.large_search_pattern_offsets:
@@ -79,7 +78,7 @@ class MotionDetection:
                             best_pos = (row2, col2)
                     stopping_condition = (match_position == best_pos)
                     match_position = (best_pos)
-                    motion_map[match_position] += motion_distance
+                    motion_map[match_position] += 1
 
                 # small offset search, small diamond
                 min_diff = float('inf')
@@ -94,48 +93,18 @@ class MotionDetection:
                     if(diff < min_diff):
                         min_diff = diff
                         best_pos = (row2, col2)
-                if(best_pos == match_position):
-                    motion_distance += 1
+                if(best_pos != match_position):
+                    shift_distance += 1
                 match_position = (best_pos)
-
-                # if(not row%100 and not col%100):
-                #     print(f"analyzing position  [{row},{col}] matches with [{match_position[0]},{match_position[1]}]")
 
                 # Compute the amount of movement
                 # For now, just add 1 to all terminating points
-                motion_map[match_position] += motion_distance
-                # motion_map[match_position] += 2**motion_distance
-                # motion_map[match_position] += 3**motion_distance
+                motion_map[match_position] += 1
+
+
 
         print("Motion computation finished")
-        # Spread movement to all the surroundings
+        # Normalize motion map
         map_max = np.max(motion_map)
-        # bw = gaussian_filter(bw, sigma=.5, mode='constant')
-        # gfilter = cv2.getGaussianKernel(self.block_size, ((self.block_size-1)/6))
-        thresh = map_max/3
-        def thresher(x): return (x*(255/map_max)) if x > thresh else 0
-        vthresher = np.vectorize(thresher)
-        bw = vthresher(motion_map)
-        # bw = (bw*(255/map_max)).astype('uint8', copy=False)
-        bw = bw.astype('uint8', copy=False)
-        bw2 = bw
-        gauss_rounds = 5
-        for _ in range(gauss_rounds):
-            bw2 = cv2.GaussianBlur(
-                bw2, (self.block_size, self.block_size), ((self.block_size-1)/6))
-        # map_max = np.max(bw2)
-        # bw2 = (bw2*(255/map_max)).astype('uint8', copy=False)
-        # np.savetxt('motion_map', bw, fmt='%d')
-        # np.savetxt('motion_map_gauss', bw2, fmt='%d')
-        # _, bw = cv2.threshold(bw, 178, 255, cv2.THRESH_BINARY)
-        # with gaussian convolution
-        # bw = bw.astype('int8')
-
-        if not original is None:
-            plt.subplot(2, 2, (1,2)), plt.imshow(
-                cv2.cvtColor(original, cv2.COLOR_BGR2RGB))
-            plt.title('Original')
-            plt.subplot(2, 2, 3), plt.imshow(bw, 'gray')
-            plt.subplot(2, 2, 4), plt.imshow(bw2, 'gray')
-            plt.title('Saliency map')
-            plt.show()
+        motion_map = motion_map/map_max
+        return motion_map.astype('uint8', copy=True)
