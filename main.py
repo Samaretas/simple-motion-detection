bebaaa944d1ee70bcd75733e14716a29b0fe36f9
Samaretas <samuele.conti@studenti.unitn.it>
2021-04-12 20:05:08 +0200

    Plotting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from cv2 import cv2
+import os
 import numpy as np
+from cv2 import cv2
 import matplotlib.pyplot as plt
 from motion_detection import MotionDetection
 
@@ -23,18 +24,17 @@ frames, colored_frames = read_video_stream('rolling_resized.mp4')
 # frames, colored_frames = read_video_stream('Rolling-resized1over4.m4v')
 print(f'read {len(frames)} frames, of size {frames[0].shape}')
 
-fidx = 30
-fidx2 = fidx+2
-
 detector = MotionDetection()
-motion_map = detector.diamond_search_motion_estimation(frames[fidx], frames[fidx2])
 
-plt.subplot(2, 2, 1), plt.imshow(cv2.cvtColor(colored_frames[fidx], cv2.COLOR_BGR2RGB))
-plt.subplot(2, 2, 2), plt.imshow(cv2.cvtColor(colored_frames[fidx2], cv2.COLOR_BGR2RGB))
-plt.title('Original')
+for i in range(len(frames)-1):
+    motion_map = detector.diamond_search_motion_estimation(frames[i], frames[i+1])
 
-plt.subplot(2, 2, (3,4)), plt.imshow(motion_map, 'gray')
-np.savetxt('motion_map', motion_map, fmt='%f')
-plt.title('Motion map')
+    plt.subplot(2, 2, 1), plt.imshow(cv2.cvtColor(colored_frames[i], cv2.COLOR_BGR2RGB))
+    plt.subplot(2, 2, 2), plt.imshow(cv2.cvtColor(colored_frames[i+1], cv2.COLOR_BGR2RGB))
+    plt.title('Original')
 
-plt.show()
+    plt.subplot(2, 2, (3,4)), plt.imshow(motion_map, 'gray')
+    plt.title('Motion map')
+    if not os.path.exists('./plots/'):
+        os.makedirs('./plots/')
+    plt.savefig('./plots/frames'+str(i)+'-'+str(i+1)+'.png')
