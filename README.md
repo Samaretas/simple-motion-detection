43eabb55acfdd0f10072678ce132ce32cfea39ca
Samaretas <samuele.conti@studenti.unitn.it>
2021-04-12 14:14:07 +0200

    Adding credits to Diamond Search

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Simple motion detection
-This repo contains a fast and simple implementation of block matching motion detection in Python.
+This repo contains a fast and simple implementation of block-matching motion detection in Python.
+
+The algorithm is an implementation of Diamond Search block-matching motion estimation adapted for motion detection purposes.
+
+To learn about Diamond Search algorithm refer to:
+
+`Shan Zhu and Kai-Kuang Ma, "A new diamond search algorithm for fast block-matching motion estimation," in IEEE Transactions on Image Processing, vol. 9, no. 2, pp. 287-290, Feb. 2000, doi: 10.1109/83.821744.`
--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 from motion_detection import MotionDetection
 
-frames = list()
-colored_frames = list()
+def read_video_stream(name):
+    frames = list()
+    colored_frames = list()
+    cap= cv2.VideoCapture(name)
+    i=0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        colored_frames.append(frame)
+        frames.append(np.array(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), dtype='uint8'))
+    cap.release()
+    return frames, colored_frames
 
-# cap= cv2.VideoCapture('moving-mouse.mp4')
-cap= cv2.VideoCapture('rolling_resized.mp4')
-i=0
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    colored_frames.append(frame)
-    frames.append(np.array(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), dtype='uint8'))
- 
-cap.release()
-cv2.destroyAllWindows()          
 
+frames, colored_frames = read_video_stream('rolling_resized.mp4')
 print(f'read {len(frames)} frames, of size {frames[0].shape}')
-cv2.imshow("Frame 30", colored_frames[30])
-cv2.imshow("Frame 31", colored_frames[31])
-# cv2.waitKey(0)
 
 detector = MotionDetection()
 detector.diamond_search_motion_estimation(frames[30], frames[31], colored_frames[30])
